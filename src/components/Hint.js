1f3b15ab99f34a0f490b3e013833f4657669d439
nathiyaayyatdurai <102350008+nathiyaayyatdurai@users.noreply.github.com>
2022-08-12 22:13:53 +0530

    'initial'

---
+++ src/components/Hint.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Hint() {
+  return (
+    <>
+      <div className="container text-center mt-5">Hint for login</div>
+      <p className="text-center">
+        email:nathiyaayyadurai.com <br />
+        password:pass@123
+      </p>
+      <p className="text-center">
+        email:akalya139@gmail.com <br />
+        password:pass@123
+      </p>
+      <p className="text-center">
+        email:nathiyakarthi.com <br />
+        password:pass@123
+      </p>
+    </>
+  );
+}
+
+export default Hint;
